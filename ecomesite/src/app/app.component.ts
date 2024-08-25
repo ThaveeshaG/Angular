ee0671295c32f56c864af31c6784a5c197ad3045
thaveesha galagama <thaveesha.g@wemakeplatforms.io>
2024-08-26 00:39:40 +0530

    add some images and creat home

--- ecomesite/src/app/app.component.ts
+++ ecomesite/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Components/navbar/navbar.component";
 import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,FooterComponent],
+  imports: [RouterOutlet, NavbarComponent,FooterComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
