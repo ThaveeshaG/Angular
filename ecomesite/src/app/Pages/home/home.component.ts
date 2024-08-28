80833bd8cc83a10b25f5efdacca33517989f8145
thaveesha galagama <thaveesha.g@wemakeplatforms.io>
2024-08-28 23:02:48 +0530

    slider and others

--- ecomesite/src/app/Pages/home/home.component.ts
+++ ecomesite/src/app/Pages/home/home.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+currentIndex = 0;
+// prevSlide() {
+//   const slider = document.getElementById('image-slider-container');
+//   if (slider) {
+//     const sliderWidth = slider.offsetWidth;
+//     this.currentIndex = (this.currentIndex - 1 + slider.children.length) % slider.children.length;
+//     slider.style.transform = `translateX(-${this.currentIndex * sliderWidth}px)`;
+//   }
+// }
+
+
+//     nextSlide() {
+//       const slider = document.getElementById('image-slider-container');
+//       if (slider) {
+//         const sliderWidth = slider.offsetWidth;
+//         this.currentIndex = (this.currentIndex + 1) % slider.children.length;
+//         slider.style.transform = `translateX(-${this.currentIndex * sliderWidth}px)`;
+//       }
+//     }
 
 }
